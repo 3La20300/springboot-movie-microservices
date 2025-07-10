3090009cc16ae5a7bfe765196b34f8d39fffb991
Moustafa Alaa Eldin Hassan Mohamed <moustafaalaa30@gmail.com>
2025-07-10 12:29:47 +0300

    Calls using Webclient library / RestTemplate  and Bing to centralize objects

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,25 +1,62 @@
 package io.javabrains.moviecatalogservice.resources;
 
 import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.Movie;
+import io.javabrains.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+import static com.fasterxml.jackson.databind.cfg.CoercionInputShape.Array;
 import static java.util.Collection.*;
+import static org.springframework.web.reactive.function.client.WebClient.*;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userID}")
     public List<CatalogItem> getCatalog(@PathVariable("userID") String userID)
     {
-        return Collections.singletonList(
-          new CatalogItem("Tranformers","Test", 4)
+
+
+        //get all rated movies IDs
+        List<Rating> ratings= Arrays.asList(
+            new Rating("1234", 4),
+            new Rating("5678", 3)
         );
+        return ratings.stream().map(rating->{
+            Movie movie=restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+//            Movie movie = webClientBuilder.build()
+//                    .get()
+//                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+//                    .retrieve()
+//                    .bodyToMono(Movie.class)
+//                    .block();
+           return new CatalogItem(movie.getName(), "DESC", rating.getRating());
+        })
+        .collect(Collectors.toList());
+
+
+        //for each movie ID, call movie info services and get details
+
+        //put them all together
+
     }
 }
