95e12ca48542235c5abfa7b05f927ca33fc1dd97
Moustafa Alaa Eldin Hassan Mohamed <moustafaalaa30@gmail.com>
2025-07-10 09:49:15 +0300

    All Services have working model ,resoucre class and right port now

--- rating-data-service/src/main/java/io/javabrains/ratingdataservice/RatingDataServiceApplication.java
+++ rating-data-service/src/main/java/io/javabrains/ratingdataservice/RatingDataServiceApplication.java
@@ -3,6 +3,7 @@ package io.javabrains.ratingdataservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class RatingDataServiceApplication {
 
