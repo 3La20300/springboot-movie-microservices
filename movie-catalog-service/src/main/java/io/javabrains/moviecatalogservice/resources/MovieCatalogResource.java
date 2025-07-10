2a93924c6506afcaf23149e6ede18fad93d0d2bf
Moustafa Alaa Eldin Hassan Mohamed <moustafaalaa30@gmail.com>
2025-07-10 04:07:37 +0300

    Models and Catalog Service API are ready

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,25 @@
+package io.javabrains.moviecatalogservice.resources;
+
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Collection.*;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userID}")
+    public List<CatalogItem> getCatalog(@PathVariable("userID") String userID)
+    {
+        return Collections.singletonList(
+          new CatalogItem("Tranformers","Test", 4)
+        );
+    }
+}
